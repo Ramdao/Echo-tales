86ad831e8c06aa7c8b4f89708fde5519af173d23
Tashrif Radin Ali <radin00@hotmail.com>
2025-04-14 13:52:23 -0400

    download page

--- instuctions.js
+++ instuctions.js
@@ -1,5 +1,5 @@
 document.querySelector(".downlaodbtn").addEventListener("click", () => {
-   
+  window.open("https://drive.google.com/file/d/134Cxk9yqpTkGW-lpl1jxBEckzoa_G0uf/view?usp=sharing", "_blank");
   });
   
   document.querySelector(".startbtn2").addEventListener("click", () => {
